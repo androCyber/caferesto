83c618ff1e1cb91046658940d88bac39e63e2fa2
Abdullah TATLICAK <tatlicak@gmail.com>
2022-02-18 20:54:16 +0300

    initial comment

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="tr">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CafeResto Projesi</title>
+    <link rel="stylesheet" href="assets/css/bootsrap.css">
+  </head>
+  <body>
+	<script src="assets/js/jquery.js"></script>
+  </body>
+</html>
