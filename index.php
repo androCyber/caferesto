84083926c3de82f13f1cc126ca3ae48e341cfe6d
Abdullah TATLICAK <tatlicak@gmail.com>
2022-02-20 07:53:57 +0300

    Tasarımın ilk aşaması ve oturtturma

--- index.php
+++ index.php
@@ -4,10 +4,37 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>CafeResto Projesi</title>
     <link rel="stylesheet" href="assets/css/bootsrap.css">
+    <title>CafeResto Projesi</title>
+    <style>
+
+  #rows {              
+  height: 32px;
+  }
+
+  #masa {
+          height: 80px;
+          margin: 12px;
+        }
+    </style>
+    
   </head>
   <body>
+          <div class="container-fluid "> <!-- Ana div başlangıç-->
+              <div class="row table-dark" id="rows"> <!-- Üst Bilgi Bannerı başlangıç-->
+               <div class="col-md-3 border-right">s</div>
+               <div class="col-md-3 border-right">s</div>
+               <div class="col-md-3 border-right">s</div>
+               <div class="col-md-3 border-right">s</div>
+              </div> <!-- Üst Bilgi Bannerı bitiş-->
+              
+                        <div class="row"><!-- Masa alanı başlangıç-->
+                        <div class="col-md-1 border" id="masa">s</div>
+                        </div> <!-- Masa alanı bitiş -->
+
+          </div><!-- Ana div bitiş-->
+
+
 	<script src="assets/js/jquery.js"></script>
   </body>
 </html>
