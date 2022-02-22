b0d18ea1fe1dcb4d73f8e5aad1cde9e6ae40af5b
Abdullah TATLICAK <tatlicak@gmail.com>
2022-02-22 16:14:18 +0300

    11. Fonksiyon yazımı , iskelet oluşturma

--- functions/functions.php
+++ functions/functions.php
@@ -4,14 +4,27 @@ $db->set_charset("utf8");
 
 class sistem {
 
+            private function myQuery($db,$query,$option)
+            {
+                    $queryPrep=$db->prepare($query);
+                    $queryPrep->execute();
+                    if($option==1):
+                   return $queryResult= $queryPrep->get_result();
+                   
+                    endif;
+                
+            }
+
+
+
+
+
+
    function masaCek($dv){
 
 
                     $masalar="Select * From masalar";
-                    $masaS=$dv->prepare($masalar);
-                    $masaS->execute();
-                    $masaRSLT=$masaS->get_result();
-                   
+                    $masaRSLT=$this->myQuery($dv,$masalar,1);
                     while($result=$masaRSLT->fetch_assoc()) :
 
                     echo ' <div class="col-md-2 col-sm-6 mr-2 mx-auto p-2 text-center text-white" >
